a621cb2553c5cc571b9f33eab19fd7d34e6891ab
Pleshz <pleskovd96@gmail.com>
2026-02-21 13:44:16 +0500

    Реализовал шифрование пароля. Плешков

--- ajax/regin_user.php
+++ ajax/regin_user.php
@@ -9,6 +9,8 @@
 
 	if($CheckPassword == false)
 		exit;
+
+	$password = password_hash($password, PASSWORD_DEFAULT);
 	
 	$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."'");
 	$id = -1;
